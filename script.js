1d30742318e415c7c1924490381101a0d2620e04
ViktoriiaSV <viktoriiasytnyk@gmail.com>
2019-02-12 23:22:34 +0200

    Done.

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function myFunc ()
+{
+with (document) 
+getElementById ('return').value = getElementById ('credit').value / 100 * getElementById ('percent').value * getElementById ('time').value;
+}
+
+
